d21ecf912d4c16cbcd6c0c281628d8448c71fbde
Greani <dominik.grueneis@gmail.com>
2017-04-27 15:08:34 +0200

    My code is better!!!

--- juliahauptmann_nikolaushofer_android/MainActivity.java
+++ juliahauptmann_nikolaushofer_android/MainActivity.java
@@ -1,17 +1,19 @@
-
+﻿
 public class Franz {
 
 	public void Irmgard(Object Ignaz){
 		ignaz.feed(new Beer(2));
+		ingnaz.rename("Normaler Hund");
 	}
 
 	public void Brunhilde(Drink beer){
 		beer.setBrand(“Wieselburger”);
+		beer.setBrand("Baumgartner");
 	}
 	
 	private booleam shouldThrowUp (int beerCount){
 		Line line = new Line();
-		if (beeCount > 20){
+		if (beerCount > 35){
 			return true;
 		} else{
 			return false;
